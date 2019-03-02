15ec8cfb42021979dcaa59465dcc6a73e0ab7337
Przemek Nowak <przemek.nowak.pl@gmail.com>
2019-03-02 21:45:15 +0100

    Fix fibonacci

--- src/test/java/pl/sda/programming/day1/Task10.java
+++ src/test/java/pl/sda/programming/day1/Task10.java
@@ -8,8 +8,8 @@ class Task10 {
 
     /**
      * Napisz program który wyświetli N elementów ciągu fibonacciego
-     *  - Do każdego wypisanego elementu wypisz jego stosunek do poprzedniego elementu ( poprzedni / aktualny )
-     *  - Ile elementów ciągu jesteś w stanie obliczyć używając zmiennych typu int? long? BigInteger?
+     * - Do każdego wypisanego elementu wypisz jego stosunek do poprzedniego elementu ( poprzedni / aktualny )
+     * - Ile elementów ciągu jesteś w stanie obliczyć używając zmiennych typu int? long? BigInteger?
      */
     @Test
     void task() {
@@ -25,34 +25,27 @@ class Task10 {
         // using int I can print 44 fibonacci numbers
         // using long I can print 90 fibonacci numbers
         // using BigInteger I printed 10000 fibonacci numbers and could probably print many more
+
     }
 
-    public static int fibonacci(int i) {
-        if(i == 0) {
-            return 1;
-        } else if(i == 1) {
-            return 1;
+    public static int fibonacci(int n) {
+        if (n <= 1) return n;
+        else return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static BigInteger fibonacci_iterative(int n) {
+        if (n <= 1) {
+            return BigInteger.valueOf(n);
         } else {
-            return fibonacci(i - 1) + fibonacci(i - 2);
-        }
-    }
-
-    public static BigInteger fibonacci_iterative(int i) {
-        if (i == 0) {
-            return BigInteger.ONE;
-        } else if(i == 1) {
-            return BigInteger.ONE;
-        }
-        else {
-            BigInteger a = BigInteger.ONE;
-            BigInteger b = BigInteger.ONE;
+            BigInteger fib = BigInteger.ONE;
+            BigInteger prevFib = BigInteger.ONE;
             BigInteger temp;
-            for (int j = 0; j < i; j++) {
-                temp = a.add(b);
-                a = b;
-                b = temp;
+            for (int i = 2; i < n; i++) {
+                temp = fib;
+                fib = fib.add(prevFib);
+                prevFib = temp;
             }
-            return b;
+            return fib;
         }
     }
 
