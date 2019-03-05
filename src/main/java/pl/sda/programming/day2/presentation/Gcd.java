7d6f144bd4087f1d20beff12a04425a2719792c0
Przemek Nowak <przemek.nowak.pl@gmail.com>
2019-03-05 21:02:44 +0100

    Simplify GDC

---
+++ src/main/java/pl/sda/programming/day2/presentation/Gcd.java
@@ -0,0 +1,27 @@
+package pl.sda.programming.day2.presentation;
+
+public class Gcd {
+
+    public static void main(String... args) {
+        System.out.println(iterativeGdc(255, 102));
+        System.out.println(recursiveGcd(255, 102));
+    }
+
+    public static int iterativeGdc(int a, int b) {
+
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a;
+    }
+
+    public static int recursiveGcd(int a, int b) {
+        int newA = a > b ? a - b : a;
+        int newB = b > a ? b - a : b;
+        return a == b ? a : recursiveGcd(newA, newB);
+    }
+}
